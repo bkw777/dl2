a4078958ee40967d7366a48858169e01ea886f79
bkw777 <b.kenyon.w@gmail.com>
2023-12-19 16:47:38 -0500

    co2ba Installing... message, remove comment

--- co2ba.sh
+++ co2ba.sh
@@ -6,14 +6,14 @@ CO_IN="$1" ;shift
 ACTION="${1^^}" ;shift
 COMMENT="$@"
 
-CO="${CO_IN##*/}" ;CO="${CO:0:6}.CO"
+CO="${CO_IN##*/}" ;CO="${CO:0:6}" ;CO="${CO%%.*}.CO"
 BYTES_PER_DATA_LINE=120
 typeset -ra h=({a..p})  # hex data output alphabet
 
 typeset -i i t b c SUM TOP END EXE LEN LINE
 typeset -a d=()
 
-abrt () { printf '%s: Usage\n%s IN.CO [call|exec|savem|bsave] ["comment..."] > OUT.DO\n%s\n' "$0" "${0##*/}" "$@" >&2 ;exit 1 ; }
+abrt () { printf '%s: Usage\n%s IN.CO [call|exec|savem|bsave] > OUT.DO\n%s\n' "$0" "${0##*/}" "$@" >&2 ;exit 1 ; }
 
 # read a binary file into to global int array d[]
 ftoi () {
@@ -31,18 +31,18 @@ ftoi () {
 ftoi "$CO_IN"
 
 # parse & discard the .CO header
-((TOP=${d[1]}*256+${d[0]}))
-((LEN=${d[3]}*256+${d[2]}))
-((END=TOP+LEN-1))
-((EXE=${d[5]}*256+${d[4]}))
+((TOP=${d[0]}+${d[1]}*256))
+((LEN=${d[2]}+${d[3]}*256))
+((EXE=${d[4]}+${d[5]}*256))
 d=(${d[*]:6})
 ((LEN==${#d[*]})) || abrt "Corrupt .CO file\nheader declares LEN=$LEN\nfile has ${#d[*]} bytes after header"
+((END=TOP+LEN-1))
 SUM= ;for ((i=0;i<LEN;i++)) { ((SUM+=${d[i]})) ; }
 
 # BASIC loader
 LINE=0
-printf '%u%c%s - loader: co2ba.sh b.kenyon.w@gmail.com %(%F)T \r' $LINE "'" "${COMMENT:-$CO}" -1
-printf '%uCLEAR256,%u:A=%u:S=0:N$="%s":CLS:?"Loading "N$" installer...";\r' $((LINE++)) $TOP $TOP "$CO"
+printf '%u%c%s - loader: co2ba.sh b.kenyon.w@gmail.com %(%F)T \r' $LINE "'" "$CO" -1
+printf '%uCLEAR256,%u:A=%u:S=0:N$="%s":CLS:?"Installing "N$" ...";\r' $((LINE++)) $TOP $TOP "$CO"
 printf '%uD$="":READD$:FORI=1TOLEN(D$)STEP2:B=(ASC(MID$(D$,I,1))-%u)*16+ASC(MID$(D$,I+1,1))-%u:POKEA,B:A=A+1:S=S+B:NEXT:?".";:IFA<%uTHEN%u\r' $LINE "'${h[0]}" "'${h[0]}" $((END+1)) $((LINE++))
 printf '%uIFS<>%uTHEN?"Bad Checksum":END\r' $((LINE++)) $SUM
 
