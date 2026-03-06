09e2820b8229090e672bd20cfd866e19813de3fd
bkw777 <b.kenyon.w@gmail.com>
2026-03-06 07:24:06 -0500

    make unsafe list configurable, add sanity checking for configurables, slightly more compact basic

--- co2ba.sh
+++ co2ba.sh
@@ -7,8 +7,11 @@ LANG=C
 : ${FIRST:=0}
 : ${LINE_GAP:=1}
 : ${LINE_LEN:=256}
+
+# SIGIL, SIGIL+SHIFT, and any UNSAFE+SHIFT must not equal any UNSAFE nor SIGIL itself nor exceed 255.
 : ${SHIFT:=64}
-: ${SIGIL:='!'}    # any of these work: ~!#$%&*`-+=/?,.|:;'<>  THESE FAIL: @^\_
+: ${SIGIL:='!'}
+: ${UNSAFE:=0 1 2 3 4 5 6 7 8 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 34}
 
 CO_IN=$1 ;shift
 ACTION=${1^^} ;shift
@@ -16,7 +19,11 @@ ACTION=${1^^} ;shift
 CO=${CO_IN##*/} ;CO=${CO:0:6} ;CO="${CO%%.*}.CO"
 
 typeset -i i b e SUM TOP END EXE LEN n g
-typeset -a d=()
+typeset -ia d=()
+
+printf -v e '%u' "'${SIGIL}"
+readonly e g=$LINE_GAP u=",${UNSAFE// /,},$e,"
+n=$FIRST
 
 abrt () { printf '%s: Usage\n%s IN.CO [call|exec|savem|bsave] > OUT.DO\n%s\n' "$0" "${0##*/}" "$@" >&2 ;exit 1 ; }
 
@@ -32,6 +39,11 @@ ftoi () {
 # help
 [[ "$CO_IN" ]] || abrt
 
+# sanity check SHIFT SIGIL UNSAFE
+b=0 ;for i in $UNSAFE $e ;do ((i>b)) && b=$i ;done
+((SHIFT>b)) || abrt "SHIFT ($SHIFT) must be higher than the highest UNSAFE ($b)"
+(((b+SHIFT)<256)) || abrt "Highest UNSAFE ($b) + SHIFT ($SHIFT) must not exceed 255"
+
 # read the .CO file into d[]
 ftoi "$CO_IN"
 
@@ -44,25 +56,27 @@ d=(${d[*]:6})
 ((END=TOP+LEN-1))
 SUM= ;for ((i=0;i<LEN;i++)) { ((SUM+=${d[i]})) ; }
 
+# Stephen Adolh encoding scheme:
+# Most bytes just copy input to output without change.
+# Unsafe bytes, add a shift value, output sigil and shifted byte.
+
 # loader
-n=$FIRST g=${LINE_GAP}
 printf '%u%c%s - loader: co2ba.sh b.kenyon.w@gmail.com %(%F)T\r' $n "'" "$CO" -1
-printf '%uREADT:CLEAR2,T:DEFINTI,O,C,V,L:DEFSNGA,K,S,T,X,E:DEFSTRB,M,D,N:READT,L,X,K,N,O,M:E=T+L-1:A=T:S=0:C=0:CLS:PRINT"Installing "N\r' $n
-printf '%uPRINT@20,CINT((L-(E-A))/L*100)"%%":READD:FORI=1TOLEN(D):B=MID$(D,I,1):IFB=MTHENC=O:NEXT:ELSEV=ASC(B)-C:POKEA,V:C=0:A=A+1:S=S+V:NEXT:IFA<=ETHEN%u\r' $((++n*g)) $n
-printf '%uPRINT:IFS<>KTHENPRINT"Bad Checksum":END\r' $((++n*g))
+printf '%uREADF:CLEAR2,F:DEFINTA-E:DEFSNGF-K:DEFSTRL-O:READF,A,J,G,N,E,M:C=0:I=F:H=F+A-1:K=0:D=0:CLS:PRINT"Installing "N\r' $n
+printf '%uPRINT@20,USING"###%%";(I-F)*100/A:READL:FORC=1TOLEN(L):O=MID$(L,C,1):IFO=MTHEND=E:NEXT:ELSEB=ASC(O)-D:POKEI,B:D=0:I=I+1:K=K+B:NEXT:IFI<=HTHEN%u\r' $((++n*g)) $n
+printf '%uPRINT:IFK<>GTHENPRINT"Bad Checksum":ELSE' $((++n*g))
 
 # action
 case "$ACTION" in
-	CALL|EXEC) printf '%u%sX\r' $((++n*g)) $ACTION ;;
-	SAVEM|BSAVE) printf '%uPRINT"Done. Please type: NEW":%sN,T,E,X\r' $((++n*g)) $ACTION ;;
-	*) printf '%uPRINT"top "T:PRINT"end "E:PRINT"exe "X\r' $((++n*g)) ;;
+	CALL|EXEC) printf '%sJ\r' $ACTION ;;
+	SAVEM|BSAVE) printf 'PRINT"Done. Please type: NEW":%sN,F,H,J\r' $ACTION ;;
+	*) printf 'PRINT"top "F:PRINT"end "H:PRINT"exe "J\r' ;;
 esac
 
 # header
 printf '%uDATA%u,%u,%u,%u,"%s",%u,"%c"\r' $((++n*g)) $TOP $LEN $EXE $SUM "$CO" $SHIFT "${SIGIL}"
 
 # data
-printf -v e '%u' "'${SIGIL}"
 O= o=
 for ((i=0;i<LEN;i++)) {
 
@@ -70,7 +84,7 @@ for ((i=0;i<LEN;i++)) {
 
 	b=${d[i]}
 
-	(( ( b<32 && b!=9 ) || b==34 || b==e )) && {
+	[[ $u = *,${b},* ]] && {
 		printf -v o '%03o' $((b+SHIFT))
 		printf -v o '%c%b' "${SIGIL}" "\\$o"
 	} || {
