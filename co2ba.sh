93c1743a1b1b47c0ca0dc29653d2cfc8523559e9
bkw777 <b.kenyon.w@gmail.com>
2026-03-14 12:15:36 -0400

    co2ba initial RLE support, also fix line numbers with LINE_GAP

--- co2ba.sh
+++ co2ba.sh
@@ -11,27 +11,32 @@ LANG=C
 : ${UNSAFE:=0 1 2 3 4 5 6 7 8 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 34}
 : ${EDITSAFE:=true}
 : ${METHOD:=Y}
-: ${ESC:=!}
+: ${EP:='!'}
 : ${XA:=^64}
 : ${XB:=^128}
 : ${YENC:=false}
 : ${CARAT:=false}
 
+: ${RLE:=false}
+: ${RP:=' '}
+
 # shorthands for some generic standards we can output
-$YENC && ESC='=' XA="+42" XB="+64"
-$CARAT && ESC='^' XA=0 XB="+64"
+$YENC && EP='=' XA="+42" XB="+64"
+$CARAT && EP='^' XA=0 XB="+64"
 $CARAT && echo "Notice: carat-encoding is broken" >&2
+$RLE && echo "(UG)RLE ENABLED  Notice: WIP: currently only works if XA=0, and only supported in METHOD=Y" >&2
 
 COFN=$1 ;shift
 ACTION=${1^^} ;shift
 PN=${COFN##*/} ;PN=${PN:0:6} ;PN=${PN%%.*}
 
-typeset -i i b esc CHK TOP END EXE LEN n g ta tb
+typeset -i i b CHK TOP END EXE LEN n g ta tb ep rp rl
 typeset -ia d=()
 
-printf -v esc '%u' "'$ESC" ;UNSAFE+=" $esc"
+printf -v ep '%u' "'$EP" ;UNSAFE+=" $ep"
+$RLE && { printf -v rp '%u' "'$RP" ;UNSAFE+=" $rp" ; }
 $EDITSAFE && UNSAFE+=" 127"
-readonly g=$LINE_GAP u=",${UNSAFE// /,}," ESC esc
+readonly g=$LINE_GAP u=",${UNSAFE// /,}," EP ep RP rp
 #ta=${XA:1} xa=true ;[[ "${XA:0:1}" = "+" ]] && xa=false ;readonly xa ta # transform A, to shift all bytes
 tb=${XB:1} xb=true ;[[ "${XB:0:1}" = "+" ]] && xb=false ;readonly xb tb # transform B, to encode unsafe bytes
 unset Ev Qd Qv
@@ -94,6 +99,36 @@ transform_a () {
 	}
 }
 
+# encode a single byte $1 to $o
+enc_o () {
+	local -i b=$1
+	[[ $u = *,$b,* ]] && {
+		$xb && b=$((b^tb)) || b=$(((b+tb)%256))
+		printf -v o '%03o' $b
+		printf -v o '%c%b' "$EP" "\\$o"
+	} || {
+		printf -v o '%03o' $b
+		printf -v o '%b' "\\$o"
+	}
+}
+
+# write a run-length to $o
+# $1 byte value
+# $2 number of copies
+rle_o () {
+	local -i b=$1 n=$2 ;local x=
+	((n>2)) && {
+		x="$RP"
+		enc_o $n
+		x+="$o"
+	} || {
+		for ((;n;n--)) {
+			enc_o $b
+			x+="$o"
+		}
+	}
+	o="$x"
+}
 
 ###############################################################################
 
@@ -124,21 +159,33 @@ case $METHOD in
 	Y) # !yenc - Adolph/B9/White yenc-like encoding
 		transform_a
 		$xb && Ev=$tb UNTB="ASC(O)XORD" || Ev=$((256-tb)) UNTB="(ASC(O)+D)MOD256"
-		printf '%uREADF:CLEAR2,F:DEFINTA-E,G,K%s:DEFSNGF,H-J:DEFSTRL-O:READF,A,J,G,N,E%s:M="%c":C=0:I=F:H=F+A-1:K=0:D=0:CLS:?"Installing "N"   0%%"\r' $n "$Qd" "$Qd" "$ESC"
-		printf '%uREADL:FORC=1TOLEN(L):O=MID$(L,C,1):IFO=MTHEND=E:NEXT:ELSEB=%s:%sPOKEI,B:D=0:I=I+1:K=(KXORB)+1:NEXT:?@18,USING"###%%";(I-F)*100/A:IFI<=HTHEN%u\r' $((++n*g)) "$UNTB" "$UNTA" $n
+
+		$RLE && { # works!!!! if XA=0, otherwise bad checksum - but even then call to the exe addr seems to work anyway
+			printf '%uREADF:CLEAR8,F:DEFINTA-E,G,K,P,S,T%s:DEFSNGF,H-J:DEFSTRL-O,R:READF,A,J,G,N,E%s:M="%c":C=0:I=F:H=F+A-1:K=0:D=0:R="%c":S=0:B=-1:P=-1:CLS:?USING"Installing \    \   0%%";N\r' $n "$Qd" "$Qd" "$EP" "$RP"
+			printf '%uREADL:FORC=1TOLEN(L):O=MID$(L,C,1):IF(O=M)THEND=E:NEXT\r' $((++n*g)) ;((l=n))
+			printf '%uIF(D=0ANDO=R)THENS=1:NEXT\r' $((++n*g))
+			printf '%uB=%s:%sD=0\r' $((++n*g)) "$UNTB" "$UNTA"
+			printf '%uIFS>0THENFORT=1TOB:POKEI,P:I=I+1:K=(KXORP)+1:NEXTT\r' $((++n*g))
+			printf '%uIFS=0THENP=B:POKEI,P:I=I+1:K=(KXORP)+1\r' $((++n*g))
+			printf '%uS=0:NEXT:?@18,USING"###%%";(I-F)*100/A:IFI<=HTHEN%u\r' $((++n*g)) $((l*g))
+		} || {
+			printf '%uREADF:CLEAR2,F:DEFINTA-E,G,K%s:DEFSNGF,H-J:DEFSTRL-O:READF,A,J,G,N,E%s:M="%c":C=0:I=F:H=F+A-1:K=0:D=0:CLS:?"Installing "N"   0%%"\r' $n "$Qd" "$Qd" "$EP"
+			printf '%uREADL:FORC=1TOLEN(L):O=MID$(L,C,1):IFO=MTHEND=E:NEXT:ELSEB=%s:%sD=0:POKEI,B:I=I+1:K=(KXORB)+1:NEXT:?@18,USING"###%%";(I-F)*100/A:IFI<=HTHEN%u\r' $((++n*g)) "$UNTB" "$UNTA" $((n*g))
+		}
+
 	;;
 	B) # Same as Y but avoids using IF in the inner loop, but actually runs slower
 		transform_a
 		$xb && Ev=$tb UNTB="BXORE*D" || Ev=$((256-tb)) UNTB="(B+E*D)MOD256"
-		printf '%uREADF:CLEAR2,F:DEFINTA-E,G,K,O-P%s:DEFSNGF,H-J:DEFSTRL-N:READF,A,J,G,N,E%s:M="":C=0:I=F:H=F+A-1:K=0:D=0:O=%u:P=0:CLS:?"Installing "N"   0%%"\r' $n "$Qd" "$Qd" $esc
+		printf '%uREADF:CLEAR2,F:DEFINTA-E,G,K,O-P%s:DEFSNGF,H-J:DEFSTRL-N:READF,A,J,G,N,E%s:M="":C=0:I=F:H=F+A-1:K=0:D=0:O=%u:P=0:CLS:?"Installing "N"   0%%"\r' $n "$Qd" "$Qd" $ep
 		# X=SGN(AXORB) could be X=-(A<>B)  but SGN(XOR) is slightly faster, 40 vs 43 seconds for 10000
-		printf '%uREADL:FORC=1TOLEN(L):B=ASC(MID$(L,C,1)):P=SGN(BXORO):B=%s:%sPOKEI,B:I=I+P:K=((KXORB)+1)*P:D=PXOR1:NEXT:?@18,USING"###%%";(I-F)*100/A:IFI<=HTHEN%u\r' $((++n*g)) "$UNTB" "$UNTA" $n
+		printf '%uREADL:FORC=1TOLEN(L):B=ASC(MID$(L,C,1)):P=SGN(BXORO):B=%s:%sPOKEI,B:I=I+P:K=((KXORB)+1)*P:D=PXOR1:NEXT:?@18,USING"###%%";(I-F)*100/A:IFI<=HTHEN%u\r' $((++n*g)) "$UNTB" "$UNTA" $((n*g))
 	;;
 	H) # hex pairs
 		typeset -ra h=({a..p})  # hex data output alphabet
 		printf -v Ev '%u' "'${h[0]}"
 		printf '%uREADF:CLEAR2,F:DEFINTA-E,G,K:DEFSNGF,H-J:DEFSTRL-N:READF,A,J,G,N,E:M="":C=0:I=F:H=F+A-1:K=0:CLS:?"Installing "N"   0%%";\r' $n
-		printf '%uREADL:FORC=1TOLEN(L)STEP2:B=(ASC(MID$(L,C,1))-E)*16+ASC(MID$(L,C+1,1))-E:POKEI,B:I=I+1:K=(KXORB)+1:NEXT:?@18,USING"###%%";(I-F)*100/A:IFI<=HTHEN%u\r' $((++n*g)) $n
+		printf '%uREADL:FORC=1TOLEN(L)STEP2:B=(ASC(MID$(L,C,1))-E)*16+ASC(MID$(L,C+1,1))-E:POKEI,B:I=I+1:K=(KXORB)+1:NEXT:?@18,USING"###%%";(I-F)*100/A:IFI<=HTHEN%u\r' $((++n*g)) $((n*g))
 	;;
 	I) # ints
 		printf '%uREADF:CLEAR2,F:DEFINTA-E,G,K:DEFSNGF,H-J:DEFSTRL-N:READF,A,J,G,N:H=F+A-1:K=0:CLS:?"Installing "N:FORI=FTOH:READB:POKEI,B:K=(KXORB)+1:?".";:NEXT:?\r' $n
@@ -159,7 +206,7 @@ esac
 printf '%uDATA%u,%u,%u,%u,"%s"%s%s\r' $((++n*g)) $TOP $LEN $EXE $CHK "$PN" "${Ev:+,$Ev}" "${Qv:+,$Qv}"
 
 # data
-O= o=
+O= o= rl=0 pb=-1
 for ((i=0;i<LEN;i++)) {
 
 	((${#O})) || {
@@ -171,22 +218,24 @@ for ((i=0;i<LEN;i++)) {
 		O+="$o"
 	}
 
-	b=${d[i]}
+	((cb=d[i]))
 
 	case $METHOD in
-		I) o=$b, ;;
-		H) o=${h[b/16]}${h[b%16]} ;;
+		I) o=$cb, ;;
+		H) o=${h[cb/16]}${h[cb%16]} ;;
 		*)
-			[[ $u = *,${b},* ]] && {
-				$xb && b=$((b^tb)) || b=$(((b+tb)%256))
-				printf -v o '%03o' $b
-				printf -v o '%c%b' "${ESC}" "\\$o"
+			$RLE && ((cb==pb && rl<255)) && {
+				: $((rl++))
 			} || {
-				printf -v o '%03o' $b
-				printf -v o '%b' "\\$o"
+				x=
+				$RLE && { rle_o $pb $rl ;rl=0 x="$o" ; }
+				enc_o $cb
+				o="$x$o"
 			}
 			;;
 	esac
+
+	((pb=cb))
 
 	((${#O}+${#o}<LINE_LEN)) && {
 		O+=$o
@@ -201,5 +250,27 @@ for ((i=0;i<LEN;i++)) {
 
 ((${#O})) && {
 	[[ "$METHOD" == "I" ]] && O=${O:0:-1}
+	$RLE && { rle_o $pb $rl ;O+="$o" ; }
 	printf '%s\r' "$O"
+}
+
+# output trailing ^Z for directly catting into BASIC without dl -b
+#printf '%b' '\032'
+
+exit
+
+# paste this function into a terminal for a quick & dirty bootstrapper
+#   $ RLE=true XA=0 co2ba.sh ALTERN.CO callba >t
+#   $ tsend t
+tsend () {
+	local d=${2:-/dev/ttyUSB0} b=${3:-9600} s=([19200]=9 [9600]=8 [4800]=7 [2400]=6 [1200]=5 [600]=4 [300]=3)
+	((${#1})) || { echo "${FUNCNAME[0]} FILE.DO [/dev/ttyX] [baud]" ;return ; }
+	[[ -c $d ]] || { echo /dev/tty* ;return ; }
+	((${#s[b]})) || { echo ${!s[*]} ;return ; }
+	echo "NEC: RUN\"COM:${s[b]}N81XN"
+	echo "100/102/200/K85/M10: RUN\"COM:${s[b]}8N1ENN"
+	read -p "Press [Enter] whean ready: " ;echo "Sending..."
+	stty -F $d $b raw pass8 clocal cread time 1 min 1 -crtscts ixon ixoff flusho -drain
+	cat $1 >$d
+	printf '%b' '\x1A' >$d
 }
